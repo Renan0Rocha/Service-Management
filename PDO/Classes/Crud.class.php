a8da797626bfeb32b244896fd5e40458e184a8f5
Renan Rocha <renanro919@gmail.com>
2022-11-11 16:22:51 -0400

    Up files

---
+++ PDO/Classes/Crud.class.php
@@ -0,0 +1,28 @@
+<?php
+    abstract class Crud{
+        protected $tabela;
+        abstract function inserir();
+        abstract function atualizar($campo, $id);
+
+        public function listar(){
+            $sqlSelect = "select * from {$this->tabela}";
+            $stmt = Conexao::prepare($sqlSelect);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        }
+
+        public function buscar($campo, $id){
+            $sqlSelect = "select * from {$this->tabela} where $campo=:parametro";
+            $stmt = Conexao::prepare($sqlSelect);
+            $stmt->bindParam(':parametro',$id,PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetch();           
+        }
+
+        public function deletar($campo, $id){
+            $sqlSelect = "delete from {$this->tabela} where $campo=:delPar";
+            $stmt = Conexao::prepare($sqlSelect);
+            $stmt->bindParam(':delPar',$id,PDO::PARAM_INT);
+            $stmt->execute();
+        }
+    }
---
+++ PDO/GerOrdem.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Nova Ordem</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <?php include_once '_parts/_header.php'; ?>
+    <div class="container mt-3">
+
+    </div>
+    <?php include '_parts/_linkJS.php'; ?>
+</body>
+</html>
