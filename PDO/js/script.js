db351afc6ed890819665848ea28617eddb59196d
Renan Rocha <renanro919@gmail.com>
2022-11-17 15:30:10 -0400

    qted

--- PDO/js/script.js
+++ PDO/js/script.js
@@ -2,6 +2,8 @@ function addServico(id, nome, valor){
     bootstrap.Modal.getOrCreateInstance(document.getElementById('modalOS')).hide();
     const tcorpo = document.getElementById('itemOS');
     const linha = document.createElement('tr');
-    linha.innerHTML="<td>"+id+"</td><td>"+nome+"</td>";
+    var inputId = '<input name="id[]" value="'+id+'" readonly>';
+    var inputQtde = '<input name="qtde[]" value="'+id+'" readonly>';
+    linha.innerHTML="<td>"+inputId+"</td><td>"+nome+"</td><td></td>" + inputQtde;
     tcorpo.appendChild(linha);
 }
